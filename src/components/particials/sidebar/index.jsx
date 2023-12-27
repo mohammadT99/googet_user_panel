0590a74cc10b588b32f540c6b17d4cfa412007c0
mohammadT99 <p.m.t.king.1999@gmail.com>
2023-12-27 17:00:22 +0330

    crate sidebar navlink

--- src/components/particials/sidebar/index.jsx
+++ src/components/particials/sidebar/index.jsx
@@ -1,49 +1,48 @@
-import React from "react"
+import React from "react";
 //  ================ image ================= //
-import Logo from "../../../assets/image/Googet-Logo.png"
+import Logo from "../../../assets/image/Googet-Logo.png";
 // =================== styles ============== //
-import "./style.scss"
+import "./style.scss";
 //================== icons imports ============= //
-import { Home, User } from "iconsax-react"
+import { Home, User } from "iconsax-react";
 // ============== comopnents ========== //
-import Avatar from "../../avatar"
-import SidebarItem from "../../sidebarItem"
-import { space } from "postcss/lib/list"
+import Avatar from "../../avatar";
+import SidebarItem from "../../sidebarItem";
+import { space } from "postcss/lib/list";
 
 const Sidebar = () => {
-    const sidebarItem = [
-        {
-            title: 'داشبورد',
-            icon: <Home /> ,
-            to: '/'
-        },
-        {
-            title : 'اطلاعات من' , 
-            icon : <User />,
-            to : '/'
-        },
-        
-    ]
-    
-    return (
-        <React.Fragment>
-            <div className="main">
-                <div className="sidebar fixed top-0 bottom-0 p-2 w-[300px] overflow-y-auto text-center ">
-                    <div className="text-2xl font-medium logo">
-                        <img src={Logo} alt="" />
-                    </div>
-                    <Avatar size="150px" type="user" />
-                    <div className="sidebar__content">
-                    {sidebarItem.map((item , i ) => {
-                        return(
-                            <SidebarItem title={item.title} key={} />
-                        )
-                    })}
-                    </div>
-                </div>
-            </div>
-        </React.Fragment>
-    )
-}
+  const sidebarItem = [
+    {
+      title: "داشبورد",
+      icon: <Home />,
+      to: "/",
+    },
+    {
+      title: "اطلاعات من",
+      icon: <User />,
+      to: "/",
+    },
+  ];
 
-export default Sidebar ;
+  return (
+    <React.Fragment>
+      <div className="main">
+        <div className="sidebar fixed top-0 bottom-0 p-2 w-[300px] overflow-y-auto text-center ">
+          <div className="text-2xl font-medium logo">
+            <img src={Logo} alt="" />
+          </div>
+          <Avatar size="150px" type="user" />
+          <div className="sidebar__content">
+            {sidebarItem.map((item, i) => {
+              return (
+                <SidebarItem title={item.title} key={i} icon={item.icon}  link={item.to}/>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Sidebar;
--- src/components/sidebarItem/index.jsx
+++ src/components/sidebarItem/index.jsx
@@ -1,12 +1,16 @@
- import React from "react";
+import React from "react";
 
- import './style.scss'
+import "./style.scss";
+import { NavLink } from "react-router-dom";
 
-const SidebarItem = ({title = '' , key = '' }) => {
-    return (
-        <i className="sidebar__item" key={key + 10}>
-            {title}
-        </i>
-    )
-}
-export default  SidebarItem ;
+const SidebarItem = ({ title = "", key = "", icon , link  }) => {
+  return (
+    <NavLink className="navlink " to={link}>
+      <i className="sidebar__item" key={key + 10}>
+        {title}
+        <span>{icon}</span>
+      </i>
+    </NavLink>
+  );
+};
+export default SidebarItem;
