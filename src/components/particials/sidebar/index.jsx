98fc0f5c67dda8f3627735b8eacc51d663f68f38
mohammadT99 <p.m.t.king.1999@gmail.com>
2024-01-21 13:51:22 +0330

    responsive design

--- src/components/particials/sidebar/index.jsx
+++ src/components/particials/sidebar/index.jsx
@@ -5,113 +5,126 @@ import Logo from "../../../assets/image/Googet-Logo.png";
 import "./style.scss";
 //================== icons imports ============= //
 import {
-  Home,
-  User,
-  Message2,
-  FolderFavorite,
-  Clock,
-  LogoutCurve,
-  Messages3,
+    Home,
+    User,
+    Message2,
+    FolderFavorite,
+    Clock,
+    LogoutCurve,
+    Messages3, SidebarLeft,
 } from "iconsax-react";
 // ============== comopnents ========== //
 import Avatar from "../../avatar";
 import SidebarItem from "../../sidebarItem";
-import { space } from "postcss/lib/list";
-import { NavLink } from "react-router-dom";
+import {space} from "postcss/lib/list";
+import {NavLink} from "react-router-dom";
 import Cookies from "js-cookie";
 
-const Sidebar = ({ type = "panel" }) => {
-  //state
-  // var _user = JSON.parse(Cookies.get("user"))
-  // _user = _user.user
+const Sidebar = ({type = "panel", addClass = ""}) => {
+    //state
+    // var _user = JSON.parse(Cookies.get("user"))
+    // _user = _user.user
 
-  //functions
-  const handleLogOut = () => {
-    Cookies.remove("user");
-    if (!Cookies.get("user")) {
-      location.replace("/login");
-    } else {
-      console.log("not");
+    //functions
+    const handleLogOut = () => {
+        Cookies.remove("user");
+        if (!Cookies.get("user")) {
+            location.replace("/login");
+        } else {
+            console.log("not");
+        }
+    };
+    // console.log(_user);
+    const sidebarItem = [
+        {
+            title: "داشبورد",
+            icon: <Home/>,
+            to: "/",
+        },
+        {
+            title: "اطلاعات من",
+            icon: <User/>,
+            to: "/profile",
+        },
+        {
+            title: "دیدگاه های من",
+            icon: <Message2/>,
+            to: "/comments",
+        },
+        {
+            title: "چت",
+            icon: <Messages3/>,
+            to: "/chat",
+        },
+        {
+            title: "علاقه مندی ها",
+            icon: <FolderFavorite/>,
+            to: "/interest",
+        },
+        {
+            title: "سوابق بازدید",
+            icon: <Clock/>,
+            to: "/recentvisit",
+        },
+    ];
+
+
+    const openSiebar = () => {
+        const element = document.getElementById('sidebar')
+        element.classList.toggle('flex')
     }
-  };
-  // console.log(_user);
-  const sidebarItem = [
-    {
-      title: "داشبورد",
-      icon: <Home />,
-      to: "/",
-    },
-    {
-      title: "اطلاعات من",
-      icon: <User />,
-      to: "/profile",
-    },
-    {
-      title: "دیدگاه های من",
-      icon: <Message2 />,
-      to: "/comments",
-    },
-    {
-      title: "چت",
-      icon: <Messages3 />,
-      to: "/chat",
-    },
-    {
-      title: "علاقه مندی ها",
-      icon: <FolderFavorite />,
-      to: "/interest",
-    },
-    {
-      title: "سوابق بازدید",
-      icon: <Clock />,
-      to: "/recentvisit",
-    },
-  ];
 
-  return (
-    <React.Fragment>
-      <div className={`main ${type}`}>
-        <div className="sidebar  top-0 bottom-0 p-2 w-[300px] overflow-y-auto text-center mt-8 ">
-          <div className="text-2xl font-medium logo">
-            <img src={Logo} alt="" />
-          </div>
-          <Avatar size="150px" type="user" />
-          <div className="sidebar__content">
-            {sidebarItem.map((item, i) => {
-              return (
-                <SidebarItem
-                  // type=""
-                  title={item.title}
-                  key={i}
-                  icon={item.icon}
-                  link={item.to}
-                />
-              );
-            })}
-          </div>
-          <div className="sidebar__content__min ">
-            {sidebarItem.map((item, i) => {
-              return (
-                <SidebarItem
-                  title={item.title}
-                  type="sideabar_min"
-                  key={i}
-                  icon={item.icon}
-                  link={item.to}
-                />
-              );
-            })}
-          </div>
-          <NavLink to="/" onClick={handleLogOut} className="sidebar__logout">
+    return (
+        <React.Fragment>
+            <div className={`main ${type} ${addClass}`}>
+                <div className="sidebar__icon__responsive sm:flex md:hidden lg:hidden" onClick={openSiebar}>
+                    <SidebarLeft/>
+                </div>
+                <div className="sidebar  top-0 bottom-0 p-2 w-[300px] overflow-y-auto text-center mt-8 " id="sidebar">
+                    <div className="sidebar__icon__responsive sm:flex md:hidden lg:hidden shadow-2xl"
+                         onClick={openSiebar}>
+                        <SidebarLeft/>
+                    </div>
+                    <div className="text-2xl font-medium logo">
+                        <img src={Logo} alt=""/>
+                    </div>
+                    <Avatar size="150px" type="user"/>
+                    <div className="sidebar__content">
+                        {sidebarItem.map((item, i) => {
+                            return (
+                                <SidebarItem
+                                    // type=""
+                                    title={item.title}
+                                    key={i}
+                                    icon={item.icon}
+                                    link={item.to}
+                                />
+                            );
+                        })}
+                    </div>
+                    <div className="sidebar__content__min ">
+                        {sidebarItem.map((item, i) => {
+                            return (
+                                <SidebarItem
+                                    title={item.title}
+                                    type="sideabar_min"
+                                    key={i}
+                                    icon={item.icon}
+                                    link={item.to}
+                                />
+                            );
+                        })}
+                    </div>
+                    <NavLink to="/" onClick={handleLogOut} className="sidebar__logout">
             <span>
-              <LogoutCurve />
+              <LogoutCurve/>
             </span>
-            <h1 className="text-sm">خروج از حساب کاربری</h1>
-          </NavLink>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+                        <h1 className="text-sm">خروج از حساب کاربری</h1>
+                    </NavLink>
+                </div>
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default Sidebar;
